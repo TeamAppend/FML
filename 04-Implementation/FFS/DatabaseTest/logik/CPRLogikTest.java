f916c0d962a9bd4b19607aaaa97dbc0ac8a3892d
Thomas Borg <thomas_borg333@hotmail.com>
2015-05-11 09:57:07 +0200

    Oprettet test klasser

---
+++ 04-Implementation/FFS/DatabaseTest/logik/CPRLogikTest.java
@@ -0,0 +1,33 @@
+package logik;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CPRLogikTest {
+
+	@Test
+	public void testCreateCPR() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListCPR() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindUniqueCPR() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteCPR() {
+		fail("Not yet implemented");
+	}
+
+}
