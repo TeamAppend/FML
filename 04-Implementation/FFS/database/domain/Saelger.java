1903687993dcfc1ec3a7b0ae027ccc7208bdf8e7
Looft <anderslooft@gmail.com>
2015-05-11 09:56:10 +0200

    english

---
+++ 04-Implementation/FFS/database/domain/Saelger.java
@@ -0,0 +1,35 @@
+package domain;
+
+public class Saelger {
+	private int saelger_id;
+	private String saelgerNavn;
+	private String rang;
+	private double beloebsGraense;
+	
+	public int getSaelger_id() {
+		return saelger_id;
+	}
+	public void setSaelger_id(int saelger_id) {
+		this.saelger_id = saelger_id;
+	}
+	public String getSaelgerNavn() {
+		return saelgerNavn;
+	}
+	public void setSaelgerNavn(String saelgerNavn) {
+		this.saelgerNavn = saelgerNavn;
+	}
+	public String getRang() {
+		return rang;
+	}
+	public void setRang(String rang) {
+		this.rang = rang;
+	}
+	public double getBeloebsGraense() {
+		return beloebsGraense;
+	}
+	public void setBeloebsGraense(double beloebsGraense) {
+		this.beloebsGraense = beloebsGraense;
+	}
+	
+	
+}
