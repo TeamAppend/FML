cd2b3cd3c430bf8df6ca131048604ff344eef4db
meilby44411 <lassemeilby444@hotmail.com>
2015-05-11 13:22:38 +0200

    UC 6 kreditværdighed

---
+++ 04-Implementation/FFS/database/dataaccess/Kreditvaerdighed.java
@@ -0,0 +1,9 @@
+package dataaccess;
+import com.ferrari.finances.dk.rki.*;
+
+public interface Kreditvaerdighed {
+
+	public Rating getKredigvaerdighed();
+		
+	public void setKreditvaerdighed(String cpr);
+}
