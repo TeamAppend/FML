cbdf1f2142740dcad99cf12fb276787668394998
Looft <anderslooft@gmail.com>
2015-05-13 12:28:07 +0200

    Thomas sexually identifies as an attack helicopter

--- 04-Implementation/FFS/Test/dataaccess/RenteSatsTest.java
+++ 04-Implementation/FFS/Test/dataaccess/RenteSatsTest.java
@@ -17,28 +17,29 @@ public class RenteSatsTest {
 	private RenteSats rs;
 	//private InterestRateTestTool testTool;
 	//private InterestRate IR;
-	private CallBack callBack;
 	
 
 	@Before
 	public void setUp() throws Exception {
 		rs = new RenteSatsImpl();
-		callBack = new CallBack(){
-			@Override
-			public void onRequestComplete() {
-				System.out.println("test");
-			}};
+	
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
 	}
 
 	@Test
 	public void testGetRenteSats() {
-		rs.setRenteSats(callBack);
-		//double d = rs.getRenteSats();
-		assertEquals(5, InterestRateTestTool.newInterestRateMock(5).todaysRate(), 0.00);
+		rs.setRenteSats(new CallBack(){
+			@Override
+			public void onRequestComplete() {
+				assertEquals(5,rs.getRenteSats(), 0.00);
+			};
+		});
+		assertEquals(5,rs.getRenteSats(), 0.00);
+		//assertEquals(5, InterestRateTestTool.newInterestRateMock(5).todaysRate(), 0.00);
 	}
 
 
