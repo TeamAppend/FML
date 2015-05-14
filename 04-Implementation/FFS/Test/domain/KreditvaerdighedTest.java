e9c07f55485fc9d6e48d180be80ca43f10464fec
Thomas Borg <thomas_borg333@hotmail.com>
2015-05-14 13:59:55 +0200

    Added KreditvaerdighedTest.

---
+++ 04-Implementation/FFS/Test/domain/KreditvaerdighedTest.java
@@ -0,0 +1,50 @@
+package domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ferrari.finances.dk.rki.Rating;
+
+import domain.CallBack;
+import domain.Kreditværdighed;
+import domain.KreditværdighedImpl;
+
+public class KreditvaerdighedTest {
+
+	private Kreditværdighed kv;
+	private boolean requestCompleted = false;
+	
+	@Before
+	public void setUp() throws Exception {
+		kv = new KreditværdighedImpl();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	@Test
+	public void testSetKreditvaerdighed() throws Exception {
+		int timeOut = 5000;
+		kv.setKreditvaerdighed("3006921611", new CallBack(){
+			@Override
+			public void onRequestComplete() {
+				requestCompleted = true;
+			};
+		});
+		while(!requestCompleted && timeOut > 0){
+			Thread.sleep(500);
+			timeOut -= 500;
+		}
+		if(requestCompleted)
+			assertEquals( Rating.valueOf("B"), kv.getKredigvaerdighed());
+			assertEquals(true, kv.getkvAcceptabel());
+			assertEquals(2, kv.getTillaegspoint());
+		if(requestCompleted == false)
+			fail("Call to RKI exceeded timeout");
+	}
+}
