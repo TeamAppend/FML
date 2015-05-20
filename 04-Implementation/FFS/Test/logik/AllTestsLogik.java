19b092dd2ae32ad7bfb258cab58521bd94677dec
Thomas Borg <thomas_borg333@hotmail.com>
2015-05-20 11:08:53 +0200

    Rodet med Test klasser.

--- 04-Implementation/FFS/Test/logik/AllTestsLogik.java
+++ 04-Implementation/FFS/Test/logik/AllTestsLogik.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({CPRLogikTest.class, KundeControllerTest.class})
+@SuiteClasses({CPRLogikTest.class, KundeControllerTest.class, PostnummerLogikTest.class})
 public class AllTestsLogik {
 
 }
